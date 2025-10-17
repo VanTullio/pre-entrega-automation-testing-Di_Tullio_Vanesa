e67c39bff482bb19b934b3476f92b7aab6e9e281
VanTullio <vanesa.dt@gmail.com>
2025-10-16 21:07:56 -0300

    Login

--- Test/Test_saudemo.py
+++ Test/Test_saudemo.py
@@ -1,12 +1,15 @@
-"""import pytest
+import pytest
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),'..')))
 
+from Utils.Helpers import loguin_saucedemo, get_driver
 
-@pytest.fixture
+@pytest.fixture #(scope = 'session') #permite que no cierre el navegador y se mantiene abierto
 def driver():
+    driver = get_driver()
+    yield driver
+    driver.quit()
 
-def test_login()
-assert
-
-def test_catalogo()
-
-def test_carrito()"""""
+def test_login(driver):
+    loguin_saucedemo (driver)
